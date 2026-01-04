be15a18d9d6e52f07228c15c9cf689d00ce5df4b
Manuel <caballeromanuel203@gmail.com>
2026-01-04 23:30:51 +0800

    updated current index to 0 for dashboard

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -14,7 +14,7 @@ class Layout extends StatefulWidget {
 }
 
 class _LayoutState extends State<Layout> {
-  int _currentIndex = 2;
+  int _currentIndex = 0;
 
   final List<PumpupAppbar> _appbars = [
     PumpupAppbar(
